3df829c5ada8cce695783a98b30d88335c390685
Tyree Christensen <tyree83@gmail.com>
2021-01-22 16:15:51 -0500

    removed my firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWgXJ7oWIJ_b_s86L7fslVtInpsWEdlIQ",
-  authDomain: "starwarsify-64d43.firebaseapp.com",
-  projectId: "starwarsify-64d43",
-  storageBucket: "starwarsify-64d43.appspot.com",
-  messagingSenderId: "182860729992",
-  appId: "1:182860729992:web:fd6a2f894a18a78d29593d"
+// TODO: add your config object
 };
 
 firebase.initializeApp(firebaseConfig);
